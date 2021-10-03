ff5980a0fa4c54e4d2d9b60c6622200a4c1aaf35
joaquinaccorsi <joaquinaccorsi@gmail.com>
2021-10-03 02:40:47 -0300

    update general

--- JS/js.js
+++ JS/js.js
@@ -1,6 +1,6 @@
 var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
+Modal.addEventListener('shown.bs.modal', function () {
+  Input.focus()
  
   })
